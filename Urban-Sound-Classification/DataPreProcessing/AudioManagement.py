fe1a93bfb012637a465b2211fdc9c243e389f5bb
Gongas <goncalo.mesteves03@gmail.com>
2024-10-23 21:44:34 +0100

    Data PreProcessing Package - Initial commit

---
+++ Urban-Sound-Classification/DataPreProcessing/AudioManagement.py
@@ -0,0 +1,51 @@
+import librosa as libr
+import numpy as np
+import pandas as pd
+
+def formatFilePath(audioFold:int, audioName:str) -> str:
+    """
+    # Description
+        -> Creates a filepath to correctly access a audio file from the UrbanSound8K dataset.
+    -----------------------------------------------------------------------------------------
+    := param: audioFold - Fold where the audio sample belong to inside the dataset.
+    := param: audioName - Audio Filename inside the dataset.
+    := return: String that points to the correct file.
+    """
+
+    # Return the file path
+    return f'./UrbanSound8K/audio/fold{audioFold}/{audioName}'
+
+def loadAudio(audioID:int, df:pd.DataFrame) -> np.ndarray:
+    """
+    # Description
+        -> Loads a audio file from the dataset.
+    -------------------------------------------
+    := param: audioID - Audio Identification Number inside the dataset.
+    := param: df - Pandas DataFrame with the dataset's metadata.
+    := return: Audio object.
+    """
+    
+    # Get the audio entry
+    df_selectedAudio = df.loc[df['fsID'] == audioID]
+
+    # Get audio name
+    audioName = df_selectedAudio['slice_file_name'][0]
+
+    # Fetch audio fold
+    audioFold = df_selectedAudio['fold'][0]
+    
+    # Get Start and End Time
+    startTime = df_selectedAudio['start'][0]
+    endTime = df_selectedAudio['end'][0]
+
+    # Compute audio duration
+    audioDuration = endTime - startTime
+    
+    # Format the File Path
+    audioFilePath = formatFilePath(audioFold, audioName)
+    
+    # Load the audio
+    audio = libr.load(audioFilePath, offset=startTime, duration=audioDuration)
+
+    # Return the Audio
+    return audio
