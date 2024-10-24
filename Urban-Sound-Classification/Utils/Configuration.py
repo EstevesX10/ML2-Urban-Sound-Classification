44021cd1cbea78b99a961383dd6429d16cfe8f60
Gongas <goncalo.mesteves03@gmail.com>
2024-10-24 10:53:28 +0100

    Added constants to the config

--- Urban-Sound-Classification/Utils/Configuration.py
+++ Urban-Sound-Classification/Utils/Configuration.py
@@ -5,6 +5,17 @@ def loadConfig() -> dict:
     ----------------------------------------------------------------------------------------------------
     := return: Dictionary with some of the important file paths of the project.
     """
+
+    # Computing Values
+    sampleRate = 22050
+    hopLength = round(sampleRate * 0.0125)
+    windowLength = round(sampleRate * 0.023)
+    timeSize = ((4*sampleRate) // (hopLength + 1))
     return {
-        'ADD':'CONTENT'
+        'DURATION':4,
+        'SAMPLE_RATE':sampleRate,
+        'HOP_LENGTH':hopLength,
+        'WINDOW_LENGTH':windowLength,
+        'N_FFT':2**10,
+        'TIME_SIZE':timeSize
     }
