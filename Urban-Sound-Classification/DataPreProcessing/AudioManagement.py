a55bca5999488b0511d8723ced2f38eaee7d9da2
Gongas <goncalo.mesteves03@gmail.com>
2024-10-24 10:47:42 +0100

    -.-

--- Urban-Sound-Classification/DataPreProcessing/AudioManagement.py
+++ Urban-Sound-Classification/DataPreProcessing/AudioManagement.py
@@ -15,25 +15,25 @@ def formatFilePath(audioFold:int, audioName:str) -> str:
     # Return the file path
     return f'./UrbanSound8K/audio/fold{audioFold}/{audioName}'
 
-def loadAudio(audioSliceName:int, audioDuration:int, df:pd.DataFrame) -> np.ndarray:
+def loadAudio(audioSliceName:int, audioDuration:int, df_audio:pd.DataFrame) -> np.ndarray:
     """
     # Description
         -> Loads a audio file from the dataset.
     -------------------------------------------
     := param: audioSliceName - Audio Identification inside the dataset.
     := param: audioDuration - Duration to be considered of the audio.
-    := param: df - Pandas DataFrame with the dataset's metadata.
+    := param: df_audio - Pandas DataFrame with the dataset's metadata.
     := return: Audio object.
     """
     
     # Get the audio entry
-    df_selectedAudio = df[df['slice_file_name'] == audioSliceName]
+    df_audio_selectedAudio = df_audio[df_audio['slice_file_name'] == audioSliceName]
 
     # Get the row index of the entry
-    idx = df_selectedAudio.index.values.astype(int)[0]
+    idx = df_audio_selectedAudio.index.values.astype(int)[0]
 
     # Fetch audio fold
-    audioFold = df_selectedAudio['fold'][idx]
+    audioFold = df_audio_selectedAudio['fold'][idx]
     
     # Format the File Path
     audioFilePath = formatFilePath(audioFold, audioSliceName)
