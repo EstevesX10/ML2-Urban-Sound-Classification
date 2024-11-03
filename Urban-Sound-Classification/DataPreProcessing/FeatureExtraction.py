7fb687e963125999e1fe6e0502caa43f1ef5d6d6
Gongas <goncalo.mesteves03@gmail.com>
2024-11-03 22:15:35 +0000

    Created a function to extract only the MFCCs from all the audio samples in order to play around with some NN

--- Urban-Sound-Classification/DataPreProcessing/FeatureExtraction.py
+++ Urban-Sound-Classification/DataPreProcessing/FeatureExtraction.py
@@ -5,6 +5,53 @@ import librosa
 from sklearn.preprocessing import (MinMaxScaler)
 from .AudioManagement import (loadAudio)
 
+def extractMFCCs(audio_df:pd.DataFrame, fold:int, config:dict, pathsConfig:dict) -> None:
+    """
+    # Description
+        -> This function helps extract the mean MFCCs from audio samples 
+        of the selected Fold on the dataset.
+    --------------------------------------------------------------------
+    := param: audio_df - Pandas Dataframe with the UrbamSound8K dataset metadata.
+    := param: fold - Fold of the audios that we want to perform feature extraction on.
+    := param: config - Dictionary with Constants used in audio processing throughout the project.
+    := param: pathsConfig - Dictionary with filepaths to help organize the results of the project.
+    := return: None, since we are merely extracting data.
+    """
+    
+    # Check if the dataframe has already been computed
+    if not os.path.exists(pathsConfig['Datasets'][f'Fold-{fold}']['MFCCs-Feature']):
+        # Initialize a List to store the extracted content
+        data = []
+
+        # Get the audio filenames from the selected fold
+        foldAudios = audio_df[audio_df['fold'] == fold]['slice_file_name'].to_numpy()
+
+        # Iterate through all the audios inside the selected fold
+        for audioFileName in foldAudios:
+            # Load the Audio
+            audio = loadAudio(df_audio=audio_df, audioSliceName=audioFileName, audioDuration=config['DURATION'], targetSampleRate=config['SAMPLE_RATE'], usePadding=True)
+    
+            # Compute and append the MFCC's
+            data.append({
+                # Audio Details
+                'audio':audioFileName,
+                'fold':fold,
+
+                # MFCC
+                'MFCC':list(np.mean(librosa.feature.mfcc(y=audio, sr=config['SAMPLE_RATE'], n_mfcc=config['N_MFCC']), axis=1)),
+                
+                # Target
+                'target':audio_df[audio_df['slice_file_name'] == audioFileName]['class'].to_numpy()[0]
+            })
+
+        # Create a DataFrame with the collected data
+        df = pd.DataFrame(data)
+
+        # Save the Dataframe
+        df.to_csv(pathsConfig['Datasets'][f'Fold-{fold}']['MFCCs-Feature'], sep=',', index=False)
+    else:
+        print(f"[Fold-{fold}] MFCC's have already been Extracted!")
+
 def extractAll1DimensionalData(audio_df:pd.DataFrame, fold:int, config:dict, pathsConfig:dict) -> None:
     """
     # Description
@@ -33,13 +80,20 @@ def extractAll1DimensionalData(audio_df:pd.DataFrame, fold:int, config:dict, pat
     
             # Compute and append the extracted features to the data list
             data.append({
-                'Audio':audioFileName,
+                # Audio Details
+                'audio':audioFileName,
+                'fold':fold,
+
+                # 1-Dimensional Features
                 'Zero-Crossing Rate':librosa.feature.zero_crossing_rate(y=audio),
                 'Spectral Centroid':librosa.feature.spectral_centroid(y=audio, sr=config['SAMPLE_RATE']),
                 'Spectral Bandwidth':librosa.feature.spectral_bandwidth(y=audio, sr=config['SAMPLE_RATE']),
                 'Spectral Flatness':librosa.feature.spectral_flatness(y=audio),
                 'Spectral Roll-off':librosa.feature.spectral_rolloff(y=audio, sr=config['SAMPLE_RATE']),
-                'RMS Energy':librosa.feature.rms(y=audio)
+                'RMS Energy':librosa.feature.rms(y=audio),
+                
+                # Target
+                'target':audio_df[audio_df['slice_file_name'] == audioFileName]['class'].to_numpy()[0]
             })
 
         # Create a DataFrame with the collected data
@@ -78,11 +132,18 @@ def extractAll2DimensionalData(audio_df:pd.DataFrame, fold:int, config:dict, pat
     
             # Compute and append the extracted features to the data list
             data.append({
-                'Audio':audioFileName,
+                # Audio Details
+                'audio':audioFileName,
+                'fold':fold,
+
+                # 2-Dimensional Features
                 'MFCC':librosa.feature.mfcc(y=audio, sr=config['SAMPLE_RATE'], n_mfcc=config['N_MFCC']),
                 'Chroma STFT':librosa.feature.chroma_stft(y=audio, n_chroma=config['N_CHROMA'], sr=config['SAMPLE_RATE'], n_fft=config['N_FFT'], hop_length=config['HOP_LENGTH'], win_length=config['WINDOW_LENGTH']),
                 'Mel Spectrogram':librosa.feature.melspectrogram(y=audio, sr=config['SAMPLE_RATE']),
-                'Spectral Contrast':librosa.feature.spectral_contrast(y=audio, sr=config['SAMPLE_RATE'])
+                'Spectral Contrast':librosa.feature.spectral_contrast(y=audio, sr=config['SAMPLE_RATE']),
+
+                # Target
+                'target':audio_df[audio_df['slice_file_name'] == audioFileName]['class'].to_numpy()[0]
             })
 
         # Create a DataFrame with the collected data
@@ -92,7 +153,9 @@ def extractAll2DimensionalData(audio_df:pd.DataFrame, fold:int, config:dict, pat
         df.to_csv(pathsConfig['Datasets'][f'Fold-{fold}']['2-Dimensional-Data'], sep=',', index=False)
     else:
         print(f"[Fold-{fold}] All the 2-Dimensional Features have already been Extracted!")
-    
+
+
+# [TODO] FINISH THE FUNCTION BELLOW TO INCLUDE ALL THE FEATURES WE WANT TO USE
 def extractImportantFeatures(audio_df:pd.DataFrame, fold:int, config:dict, pathsConfig:dict) -> None:
     """
     # Description
