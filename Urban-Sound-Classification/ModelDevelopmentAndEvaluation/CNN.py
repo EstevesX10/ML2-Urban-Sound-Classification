b4993545ed78223f36bb2d97698820d7f57a626f
Gongas <goncalo.mesteves03@gmail.com>
2024-11-09 00:38:39 +0000

    Started Developing a CNN - Not currently working - input mismatches

--- Urban-Sound-Classification/ModelDevelopmentAndEvaluation/CNN.py
+++ Urban-Sound-Classification/ModelDevelopmentAndEvaluation/CNN.py
@@ -1,5 +1,45 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import keras
+from keras import Sequential
+from keras.src.layers import (Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dense, Dropout)
 
-# CODE HERE TO DEVELOP MODELS
+class CNN(keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    # Architecture used in the "An Analysis of Audio Classification Techniques using Deep Learning Architectures" Paper
+    def create2DCNN(self, numClasses=10):
+        return Sequential([
+            Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
+            MaxPooling2D(pool_size=(2, 2)),
+            Dropout(rate=0.2),
+            Dense(units=512, activation='relu'),
+            Dropout(rate=0.2),
+            Dense(units=1024, activation='relu'),
+            Dropout(rate=0.2),
+            Dense(units=2048, activation='relu'),
+            Dense(units=numClasses, activation='softmax')
+        ])
+    
+    def create1DCNN(self, numClasses=10):
+        return Sequential([
+            Conv1D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Conv1D(filters=128, kernel_size=3, strides=1, padding='same', activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Conv1D(filters=256, kernel_size=3, strides=1, padding='same', activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Dropout(rate=0.2),
+            Dense(units=512, activation='relu'),
+            Dropout(rate=0.2),
+            Dense(units=1024, activation='relu'),
+            Dropout(rate=0.2),
+            Dense(units=2048, activation='relu'),
+            Dense(units=numClasses, activation='softmax')
+        ])
