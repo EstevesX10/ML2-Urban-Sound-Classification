49cd64ca85ec320f6553f5d845419aa9f2ef35c4
Gongas <goncalo.mesteves03@gmail.com>
2024-11-12 16:20:39 +0000

    Updated Architecture

--- Urban-Sound-Classification/ModelDevelopmentAndEvaluation/CNN.py
+++ Urban-Sound-Classification/ModelDevelopmentAndEvaluation/CNN.py
@@ -3,15 +3,16 @@ import pandas as pd
 import tensorflow as tf
 import keras
 from keras import Sequential
-from keras.src.layers import (Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dense, Dropout)
+from keras.src.layers import (Input, Flatten, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dense, Dropout)
 
 class CNN(keras.Model):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
     # Architecture used in the "An Analysis of Audio Classification Techniques using Deep Learning Architectures" Paper
-    def create2DCNN(self, numClasses=10):
+    def create2DCNN(self, input_shape, numClasses=10):
         return Sequential([
+            Input(shape=input_shape),
             Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
             MaxPooling2D(pool_size=(2, 2)),
             Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
@@ -19,6 +20,7 @@ class CNN(keras.Model):
             Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
             MaxPooling2D(pool_size=(2, 2)),
             Dropout(rate=0.2),
+            Flatten(),
             Dense(units=512, activation='relu'),
             Dropout(rate=0.2),
             Dense(units=1024, activation='relu'),
@@ -27,8 +29,9 @@ class CNN(keras.Model):
             Dense(units=numClasses, activation='softmax')
         ])
     
-    def create1DCNN(self, numClasses=10):
+    def create1DCNN(self, input_shape, numClasses=10):
         return Sequential([
+            Input(shape=input_shape),
             Conv1D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu'),
             MaxPooling1D(pool_size=2),
             Conv1D(filters=128, kernel_size=3, strides=1, padding='same', activation='relu'),
@@ -36,6 +39,7 @@ class CNN(keras.Model):
             Conv1D(filters=256, kernel_size=3, strides=1, padding='same', activation='relu'),
             MaxPooling1D(pool_size=2),
             Dropout(rate=0.2),
+            Flatten(),
             Dense(units=512, activation='relu'),
             Dropout(rate=0.2),
             Dense(units=1024, activation='relu'),
