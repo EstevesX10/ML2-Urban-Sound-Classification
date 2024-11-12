697dd974b867233f00d690761aad6651fd323572
Gongas <goncalo.mesteves03@gmail.com>
2024-11-12 18:27:33 +0000

    nothing much

--- Urban-Sound-Classification/ModelDevelopmentAndEvaluation/CNN.py
+++ Urban-Sound-Classification/ModelDevelopmentAndEvaluation/CNN.py
@@ -8,7 +8,26 @@ from keras.src.layers import (Input, Flatten, Conv1D, Conv2D, MaxPooling1D, MaxP
 class CNN(keras.Model):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
+    def create1DCNN(self, input_shape, numClasses=10):
+        return Sequential([
+            Input(shape=input_shape),
+            Conv1D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Conv1D(filters=128, kernel_size=3, strides=1, padding='same', activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Conv1D(filters=256, kernel_size=3, strides=1, padding='same', activation='relu'),
+            MaxPooling1D(pool_size=2),
+            Dropout(rate=0.2),
+            Flatten(),
+            Dense(units=512, activation='relu'),
+            Dropout(rate=0.2),
+            Dense(units=1024, activation='relu'),
+            Dropout(rate=0.2),
+            Dense(units=2048, activation='relu'),
+            Dense(units=numClasses, activation='softmax')
+        ])
+
     # Architecture used in the "An Analysis of Audio Classification Techniques using Deep Learning Architectures" Paper
     def create2DCNN(self, input_shape, numClasses=10):
         return Sequential([
@@ -19,25 +38,6 @@ class CNN(keras.Model):
             MaxPooling2D(pool_size=(2, 2)),
             Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'),
             MaxPooling2D(pool_size=(2, 2)),
-            Dropout(rate=0.2),
-            Flatten(),
-            Dense(units=512, activation='relu'),
-            Dropout(rate=0.2),
-            Dense(units=1024, activation='relu'),
-            Dropout(rate=0.2),
-            Dense(units=2048, activation='relu'),
-            Dense(units=numClasses, activation='softmax')
-        ])
-    
-    def create1DCNN(self, input_shape, numClasses=10):
-        return Sequential([
-            Input(shape=input_shape),
-            Conv1D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu'),
-            MaxPooling1D(pool_size=2),
-            Conv1D(filters=128, kernel_size=3, strides=1, padding='same', activation='relu'),
-            MaxPooling1D(pool_size=2),
-            Conv1D(filters=256, kernel_size=3, strides=1, padding='same', activation='relu'),
-            MaxPooling1D(pool_size=2),
             Dropout(rate=0.2),
             Flatten(),
             Dense(units=512, activation='relu'),
