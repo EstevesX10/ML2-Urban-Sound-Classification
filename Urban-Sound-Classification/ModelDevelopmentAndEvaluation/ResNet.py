b4ef91135d6859d4f4d384671e778b5a8a4d7ca3
PsuperX <ja435364@gmail.com>
2024-11-19 23:33:17 +0000

    tryed to fix resnet

--- Urban-Sound-Classification/ModelDevelopmentAndEvaluation/ResNet.py
+++ Urban-Sound-Classification/ModelDevelopmentAndEvaluation/ResNet.py
@@ -3,36 +3,60 @@ import tensorflow as tf
 import keras
 from keras import Sequential
 from keras.src.layers import Layer
-from keras.src.layers import Input, Conv2D, BatchNormalization
+from keras.src.layers import InputLayer, Conv2D, BatchNormalization
 from keras.src.layers import MaxPooling2D, GlobalAveragePooling2D
 from keras.src.layers import Add, ReLU, Dense
+
 
 class ResidualBlock(Layer):
     def __init__(self, filters, kernel_size=3, stride=1):
         super().__init__()
 
+        self.stride = stride
+        self.filters = filters
+
         # First Convolutional Layer with Batch Normalization and Relu
-        self.conv1 = Conv2D(filters=filters, kernel_size=kernel_size, strides=stride, padding='same', use_bias=False)
+        self.conv1 = Conv2D(
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=stride,
+            padding="same",
+            use_bias=False,
+        )
         self.bn1 = BatchNormalization()
         self.relu1 = ReLU()
 
-        self.conv2 = Conv2D(filters=filters, kernel_size=3, padding='same', strides=stride)
+        self.conv2 = Conv2D(
+            filters=filters, kernel_size=3, padding="same", strides=stride
+        )
         self.bn2 = BatchNormalization()
 
         # Define the skip connection
-        if stride != 1:
-            self.skip_connection = keras.Sequential([
-                Conv2D(filters=filters, kernel_size=1, strides=stride, padding='same', use_bias=False),
-                BatchNormalization()
-            ])
-        else:
-            # Identity function
-            self.skip_connection = lambda x, training: x
+        self.skip_connection = None
 
         self.add = Add()
         self.relu2 = ReLU()
 
-    def call(self, x:tf.Tensor, training=False):
+    def build(self, input_shape):
+        # Define the skip connection
+        if self.stride != 1 or input_shape[-1] != self.filters:
+            self.skip_connection = keras.Sequential(
+                [
+                    Conv2D(
+                        filters=self.filters,
+                        kernel_size=1,
+                        strides=self.stride**2,
+                        padding="same",
+                        use_bias=False,
+                    ),
+                    BatchNormalization(),
+                ]
+            )
+        else:
+            # Identity function
+            self.skip_connection = lambda x, training: x
+
+    def call(self, x: tf.Tensor, training=False):
         # Save x for the skip connection
         residue = self.skip_connection(x, training=training)
 
@@ -50,14 +74,17 @@ class ResidualBlock(Layer):
 
         return x
 
+
 class ResNet(tf.keras.Model):
     def __init__(self, num_classes=10):
         super().__init__()
 
-        self.conv = Conv2D(filters=64, kernel_size=7, strides=2, padding='same', use_bias=False)
+        self.conv = Conv2D(
+            filters=64, kernel_size=7, strides=2, padding="same", use_bias=False
+        )
         self.bn = BatchNormalization()
         self.relu = ReLU()
-        self.pool = MaxPooling2D(pool_size=3, strides=2, padding='same')
+        self.pool = MaxPooling2D(pool_size=3, strides=2, padding="same")
 
         # Define multiple blocks
         self.layer1 = self._buildLayer(filters=64, blocks=2, stride=1)
@@ -66,7 +93,7 @@ class ResNet(tf.keras.Model):
         self.layer4 = self._buildLayer(filters=512, blocks=2, stride=2)
 
         self.globalAvgPool = GlobalAveragePooling2D()
-        self.fullyConnectedLayer = Dense(num_classes, activation='softmax')
+        self.fullyConnectedLayer = Dense(num_classes, activation="softmax")
 
     def _buildLayer(self, filters, blocks, stride):
         # Create a list for the residual layers
@@ -78,11 +105,11 @@ class ResNet(tf.keras.Model):
         # Get all the remaining residual blocks
         for _ in range(1, blocks):
             residualLayers.append(ResidualBlock(filters=filters, stride=1))
-    
+
         # Grab all the residual layers and convert them into a Sequencial Model
         return Sequential(residualLayers)
-    
-    def call(self, x:tf.Tensor, training=False):
+
+    def call(self, x: tf.Tensor, training=False):
         # Go through the network's initial layers
         x = self.conv(x)
         x = self.bn(x)
@@ -100,4 +127,3 @@ class ResNet(tf.keras.Model):
         x = self.fullyConnectedLayer(x)
 
         return x
-
