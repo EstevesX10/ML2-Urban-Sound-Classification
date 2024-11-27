d1f27d586c914b43b386bd600a96070f3a620e56
Gongas <goncalo.mesteves03@gmail.com>
2024-11-27 14:18:04 +0000

    Merge branch 'main' of https://github.com/EstevesX10/ML2-Urban-Sound-Classification

--- Urban-Sound-Classification/Utils/Configuration.py
+++ Urban-Sound-Classification/Utils/Configuration.py
@@ -22,7 +22,8 @@ def loadConfig() -> dict:
         "N_MFCC": 13,  # Number of Mel-Frequency Cepstral Coefficients (MFCCs) to be extracted
     }
 
-def createDatasetsFolderPaths(numberFolds:int) -> dict:
+
+def createDatasetsFolderPaths(numberFolds: int) -> dict:
     """
     # Description
         -> This function helps configure all the paths in which we are going to store the extracted features.
@@ -30,7 +31,7 @@ def createDatasetsFolderPaths(numberFolds:int) -> dict:
     := param: numberFolds -  Number of folds that are going to be considered.
     := return: Dictionary with all the datasets paths configured.
     """
-    
+
     # Create a main dictionary to store all the paths
     datasetsPaths = {}
 
@@ -39,31 +40,31 @@ def createDatasetsFolderPaths(numberFolds:int) -> dict:
         currentFoldDatasetsPaths = {
             # All the Available Features to process the Audio Samples
             "All-Raw-Features": f"./Datasets/Fold-{foldIdx}/All-Raw-Features.pkl",
-
             # Files to store 1-Dimensional Features
             "1D-Raw-Features": f"./Datasets/Fold-{foldIdx}/1D-Raw-Features.pkl",
             "1D-Processed-Features": f"./Datasets/Fold-{foldIdx}/1D-Processed-Features.pkl",
-            
             # Files to store 2-Dimensional Features
             "2D-Raw-Features": f"./Datasets/Fold-{foldIdx}/2D-Raw-Features.pkl",
             "2D-Processed-Features": f"./Datasets/Fold-{foldIdx}/2D-Processed-Features.pkl",
-
-            # Files to store the MFCCs 
+            # Files to store the MFCCs
             "2D-Raw-MFCCs": f"./Datasets/Fold-{foldIdx}/2D-Raw-MFCCs.pkl",
             "1D-Processed-MFCCs": f"./Datasets/Fold-{foldIdx}/1D-Processed-MFCCs.pkl",
         }
 
         # Update the main dictionary
-        datasetsPaths.update({f"Fold-{foldIdx}":currentFoldDatasetsPaths})
-    
+        datasetsPaths.update({f"Fold-{foldIdx}": currentFoldDatasetsPaths})
+
     # Add the path for transfer learning
     datasetsPaths.update({"transfer": "./Datasets/transfer.pkl"})
-   
+
     # Return the dictionary with all the paths for the datasets
     return datasetsPaths
 
-def createModelResultsFolderPaths(modelName:str, numberTests:int, numberFolds:int) -> dict:
-    """"
+
+def createModelResultsFolderPaths(
+    modelName: str, numberTests: int, numberFolds: int
+) -> dict:
+    """ "
     # Description
         -> This function helps create all the paths to store the experimental results of a given model.
     ---------------------------------------------------------------------------------------------------
@@ -80,21 +81,24 @@ def createModelResultsFolderPaths(modelName:str, numberTests:int, numberFolds:in
     for testIdx in range(1, numberTests + 1):
         # Create a dictionary for the paths of the results from each fold for the current test
         foldsData = {}
-        
+
         # Iterate through each fold
         for foldIdx in range(1, numberFolds + 1):
             # Create the paths
-            foldsData.update({
-                f"Fold-{foldIdx}":{
-                    "History": f"./ExperimentalResults/ModelPerformanceResults/{modelName}/Test-{testIdx}/Fold-{foldIdx}/history.pkl",
-                    "Model": f"./ExperimentalResults/ModelPerformanceResults/{modelName}/Test-{testIdx}/Fold-{foldIdx}/model.keras"
+            foldsData.update(
+                {
+                    f"Fold-{foldIdx}": {
+                        "History": f"./ExperimentalResults/ModelPerformanceResults/{modelName}/Test-{testIdx}/Fold-{foldIdx}/history.pkl",
+                        "Model": f"./ExperimentalResults/ModelPerformanceResults/{modelName}/Test-{testIdx}/Fold-{foldIdx}/model.keras",
+                    }
                 }
-            })
+            )
         # Update the main dictionary
-        testsData.update({f"Test-{testIdx}":foldsData})
-    
+        testsData.update({f"Test-{testIdx}": foldsData})
+
     # Return the Paths for the model experimental results
     return testsData
+
 
 def loadPathsConfig() -> dict:
     """
@@ -107,10 +111,18 @@ def loadPathsConfig() -> dict:
         "ExploratoryDataAnalysis": "./ExperimentalResults/ExploratoryDataAnalysis",
         "Datasets": createDatasetsFolderPaths(numberFolds=10),
         "ModelDevelopmentAndEvaluation": {
-            "MLP": createModelResultsFolderPaths(modelName="MLP", numberTests=3, numberFolds=10),
-            "CNN": createModelResultsFolderPaths(modelName="CNN", numberTests=1, numberFolds=10),
-            "YAMNET": createModelResultsFolderPaths(modelName="YAMNET", numberTests=1, numberFolds=10),
-            "ResNet": createModelResultsFolderPaths(modelName="ResNet", numberTests=3, numberFolds=10),
+            "MLP": createModelResultsFolderPaths(
+                modelName="MLP", numberTests=3, numberFolds=10
+            ),
+            "CNN": createModelResultsFolderPaths(
+                modelName="CNN", numberTests=4, numberFolds=10
+            ),
+            "YAMNET": createModelResultsFolderPaths(
+                modelName="YAMNET", numberTests=1, numberFolds=10
+            ),
+            "ResNet": createModelResultsFolderPaths(
+                modelName="ResNet", numberTests=3, numberFolds=10
+            ),
             "yamnet-train": "./ExperimentalResults/ModelDevelopmentAndEvaluation/yamnet/train.pkl",
             "yamnet-test": "./ExperimentalResults/ModelDevelopmentAndEvaluation/yamnet/test.pkl",
         },
